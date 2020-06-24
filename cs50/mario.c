c62412e2b72a8562a040c4cbfa1937babb73e7c5
Matthew Austin <mattaustin44@gmail.com>
2020-06-24 09:50:01 -0500

    mario from cs50

---
+++ cs50/mario.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+//print the array
+//dir=0 means print forward direction, dir=1 means print backwards direction
+void printArr(char* arr, int dir, int size) {
+	if (dir == 0) {
+		for (int i = 0; i < size; i++) {
+			printf("%c", arr[i]);
+		}
+	} else if (dir == 1) {
+		for (int i = (size - 1); i >= 0; i--) {
+			printf("%c", arr[i]);
+		}
+	}
+}
+
+
+
+int main(void) {
+	//print the prompt and get the user input
+	int n;
+	printf("Height: ");
+	scanf("%d", &n);
+	printf("\n");
+
+	//allocate the array to store the pyramid
+	char* arr;
+	arr = malloc(n); 
+	memset(arr, ' ', n);
+
+	//loop while printing the pyramid
+	for (int i = 0; i < n; i++) {
+		arr[i] = '#';
+		printArr(arr, 1, n);
+		printf("  ");
+		printArr(arr, 0, n);
+		printf("\n");
+	}
+	return 0;
+}
+
